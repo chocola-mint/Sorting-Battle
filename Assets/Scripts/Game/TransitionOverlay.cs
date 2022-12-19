36c59e1a4ed21b617d6d85ca81dc4b40204c04f2
CHM <56677134+chocola-mint@users.noreply.github.com>
2022-12-19 12:11:09 +0800

    Update documentation in TransitionOverlay

--- Assets/Scripts/Game/TransitionOverlay.cs
+++ Assets/Scripts/Game/TransitionOverlay.cs
@@ -5,10 +5,17 @@ using UnityEngine.SceneManagement;
 using UnityEngine.Events;
 namespace SortGame
 {
+    /// <summary>
+    /// Component that handles transition effects for the scene it is in.
+    /// <br></br>
+    /// Will trigger "Entrance" transition on the first frame of activation.
+    /// The "Exit" transition will happen when <see cref="RestartScene"></see> or
+    /// <see cref="MoveToScene"></see> is invoked.
+    /// </summary>
     public class TransitionOverlay : MonoBehaviour
     {
         // ! These objects should set themselves to inactive once they are done.
-        [Tooltip("GameObjects that contain transition effects. Should turn themselves inactive after they end.")]
+        [Tooltip("Sequence of GameObjects that contain transition effects. Should turn themselves inactive after they end.")]
         [SerializeField] List<GameObject> entrance, exit;
         public UnityEvent onEntranceTransitionOver;
         private void Start() 
@@ -17,10 +24,14 @@ namespace SortGame
         }
         private IEnumerator ProcessTransitions(List<GameObject> transitionList)
         {
+            // Process transitions in sequential order.
             foreach(var transitionPrefab in transitionList)
             {
+                // Instantiate transition object.
                 var transition = Instantiate(transitionPrefab);
+                // Then wait for the transition object to turn itself off.
                 yield return new WaitUntil(() => !transition.activeSelf);
+                // Finally, destroy the transition object.
                 Destroy(transition);
             }
         }
@@ -30,7 +41,13 @@ namespace SortGame
                 yield return ProcessTransitions(entrance);
             onEntranceTransitionOver.Invoke();
         }
+        /// <summary>
+        /// Reload the current scene, triggering transitions.
+        /// </summary>
         public void RestartScene() => MoveToScene(SceneManager.GetActiveScene().name);
+        /// <summary>
+        /// Load the given scene, triggering transitions.
+        /// </summary>
         public void MoveToScene(string sceneName)
         {
             StopAllCoroutines();
@@ -38,8 +55,10 @@ namespace SortGame
         }
         private IEnumerator CoroMoveToScene(string sceneName)
         {
+            // Start the async load scene task.
             var task = SceneManager.LoadSceneAsync(sceneName);
             task.allowSceneActivation = false;
+            // Process exit transitions.
             if(exit != null)
             {
                 if(exit.Count > 0)
