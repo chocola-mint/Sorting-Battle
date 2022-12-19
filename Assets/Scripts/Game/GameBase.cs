e4bda1625676447eb2941459c4ab329dedefff8b
CHM <56677134+chocola-mint@users.noreply.github.com>
2022-12-19 12:25:21 +0800

    GameBase documentation

--- Assets/Scripts/Game/GameBase.cs
+++ Assets/Scripts/Game/GameBase.cs
@@ -4,16 +4,29 @@ using UnityEngine;
 
 namespace SortGame
 {
+    /// <summary>
+    /// Base class for all game modes based on GameState implementations.
+    /// </summary>
     public abstract class GameBase<T> : MonoBehaviour where T : GameState
     {
         protected T gameState;
         protected static readonly WaitForFixedUpdate waitForFixedUpdate = new();
         private Coroutine clockHandle;
+        /// <summary>
+        /// Invoke to start the game.
+        /// </summary>
         public abstract void StartGame();
+        /// <summary>
+        /// Invoke to start the game's timer, controlled by FixedUpdates.
+        /// </summary>
+        /// <param name="fixedUpdateToTick">FixedUpdates per tick.</param>
         protected void StartTicking(int fixedUpdateToTick) 
         { 
             clockHandle = StartCoroutine(CoroTick(fixedUpdateToTick));
         }
+        /// <summary>
+        /// Stop the ticking of the game's timer.
+        /// </summary>
         protected void StopTicking()
         {
             StopCoroutine(clockHandle);
