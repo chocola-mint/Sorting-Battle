39939108e981267f3f95c74bea3df164a5b9c77d
CHM <56677134+chocola-mint@users.noreply.github.com>
2022-12-28 19:03:30 +0800

    Use holding rather than pressing

--- Assets/Scripts/Game/UI/EscapeToQuitGame.cs
+++ Assets/Scripts/Game/UI/EscapeToQuitGame.cs
@@ -6,11 +6,14 @@ using UnityEngine.InputSystem;
 namespace SortGame.UI
 {
     /// <summary>
-    /// Utility component that enables pressing Esc to quit the game anywhere.
+    /// Utility component that enables holding Esc for <see cref="confirmTime"></see> seconds
+    /// to quit the game anywhere.
     /// Only enabled on non-web platforms.
     /// </summary>
     public class EscapeToQuitGame : MonoBehaviour
     {
+        private float waitEndTime = float.PositiveInfinity;
+        private const float confirmTime = 2.0f;
         [RuntimeInitializeOnLoadMethod]
         public static void Init()
         {
@@ -27,6 +30,11 @@ namespace SortGame.UI
         void Update()
         {
             if(Keyboard.current.escapeKey.wasPressedThisFrame)
+                waitEndTime = Time.unscaledTime + confirmTime;
+            if(Keyboard.current.escapeKey.wasReleasedThisFrame)
+                waitEndTime = float.PositiveInfinity;
+
+            if(Time.unscaledTime > waitEndTime)
             {
 #if UNITY_EDITOR
                 Debug.Log($"Quitting game via {nameof(EscapeToQuitGame)}. (Simulated as ExitPlaymode)");
